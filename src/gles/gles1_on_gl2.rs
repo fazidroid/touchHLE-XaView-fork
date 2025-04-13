upintheairsheep/touchHLE

commit f57c2c199d5f3354070d1408e2480f2eea398b23

Tolerating glCullFace(gl21::CCW)
Change-Id: I57a2e7cc133171205e6327859b49f17050693c88

src/gles/gles1_on_gl2.rs
@@ -835,8 +835,15 @@ impl GLES for GLES1OnGL2 {
         gl21::ClipPlane(plane, &equation_double as _)
     }
     unsafe fn CullFace(&mut self, mode: GLenum) {
-        assert!([gl21::FRONT, gl21::BACK, gl21::FRONT_AND_BACK].contains(&mode));
-        gl21::CullFace(mode);
+        if mode == gl21::CCW {
+            log_dbg!("Tolerating glCullFace({:#x})", mode);
+        } else {
+            assert!(
+                [gl21::FRONT, gl21::BACK, gl21::FRONT_AND_BACK].contains(&mode),
+                "Unexpected glCullFace({:#x})",
+                mode
+            );
+        }
     }
     unsafe fn DepthFunc(&mut self, func: GLenum) {
         assert!([

fazidroid/touchHLE-XaView-fork

commit 1e44583b0b6130e4be4cb53bfc86ebef9f69c439

Fix regression in glCullFace() for GLES1OnGL2
It was introduced in f57c2c199d5f3354070d1408e2480f2eea398b23

Change-Id: Id8806743f0429d29f5d5bd316fbf6b18832674bb

src/gles/gles1_on_gl2.rs
@@ -845,6 +845,7 @@ impl GLES for GLES1OnGL2 {
                 mode
             );
         }
+        gl21::CullFace(mode);
     }
     unsafe fn DepthFunc(&mut self, func: GLenum) {
         assert!([