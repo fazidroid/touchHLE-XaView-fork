8da840321abc589e79677f81535acaf6c6d7f4ca
ciciplusplus <ciciplusplus@gmail.com>
2025-07-12 22:56:20 +0200

    Simplify NSLock implementation
    It's more convenient to use our internal mutex id and the locking checks
    are already done in the underling implementation anyway.
    
    Change-Id: I3c957d4271428ccfd2f7c32569e31353dad5d368

--- src/frameworks/foundation/ns_lock.rs
+++ src/frameworks/foundation/ns_lock.rs
@@ -5,19 +5,13 @@
  */
 //! `NSLock`.
 
-use crate::environment::ThreadId;
-use crate::libc::pthread::mutex::{
-    pthread_mutex_destroy, pthread_mutex_init, pthread_mutex_lock, pthread_mutex_t,
-    pthread_mutex_unlock,
-};
-use crate::mem::{guest_size_of, MutPtr};
+use crate::environment::{MutexId, PTHREAD_MUTEX_DEFAULT};
 use crate::msg;
 use crate::objc::{id, nil, objc_classes, ClassExports, HostObject};
 
 struct NSLockHostObject {
-    pthread_mutex_ptr: MutPtr<pthread_mutex_t>,
+    mutex_id: MutexId,
     name: id,
-    locked_by: Option<ThreadId>,
 }
 impl HostObject for NSLockHostObject {}
 
@@ -29,30 +23,23 @@ pub const CLASSES: ClassExports = objc_classes! {
 
 + (id)alloc {
     log_dbg!("[NSLock alloc]");
-    let pthread_mutex_ptr = env.mem.alloc(guest_size_of::<pthread_mutex_t>()).cast();
-    assert!(pthread_mutex_init(env, pthread_mutex_ptr, nil.cast().cast_const()) == 0);
-    let host_object = NSLockHostObject { pthread_mutex_ptr, name: nil, locked_by: None };
+    let mutex_id = env.mutex_state.init_mutex(PTHREAD_MUTEX_DEFAULT);
+    let host_object = NSLockHostObject { mutex_id, name: nil };
     env.objc.alloc_object(this, Box::new(host_object), &mut env.mem)
 }
 
 - (())lock {
     log_dbg!("[(NSLock*){:?} lock]", this);
     let host_object = env.objc.borrow::<NSLockHostObject>(this);
-    assert!(host_object.locked_by.is_none());
-    assert!(pthread_mutex_lock(env, host_object.pthread_mutex_ptr) == 0);
-    env.objc.borrow_mut::<NSLockHostObject>(this).locked_by = Some(env.current_thread);
+    env.lock_mutex(host_object.mutex_id).unwrap();
 }
 
 - (())unlock {
     log_dbg!("[(NSLock*){:?} unlock]", this);
     let host_object = env.objc.borrow::<NSLockHostObject>(this);
-    if let Some(locked_by_thread) = host_object.locked_by {
-        assert!(locked_by_thread == env.current_thread);
-    } else {
+    if !env.mutex_state.mutex_is_locked(host_object.mutex_id) {
         echo!("*** -[NSLock unlock]: lock (<NSLock: {:?}> '{:?}') unlocked when not locked", this, host_object.name);
     }
-    assert!(pthread_mutex_unlock(env, host_object.pthread_mutex_ptr) == 0);
-    env.objc.borrow_mut::<NSLockHostObject>(this).locked_by = None
 }
 
 - (())setName:(id)name { // NSString *
@@ -65,8 +52,8 @@ pub const CLASSES: ClassExports = objc_classes! {
 
 - (())dealloc {
     log_dbg!("[(NSLock*){:?} dealloc]", this);
-    let pthread_mutex_ptr = env.objc.borrow::<NSLockHostObject>(this).pthread_mutex_ptr;
-    assert!(pthread_mutex_destroy(env, pthread_mutex_ptr) == 0);
+    let host_object = env.objc.borrow::<NSLockHostObject>(this);
+    env.mutex_state.destroy_mutex(host_object.mutex_id).unwrap();
     env.objc.dealloc_object(this, &mut env.mem)
 }
 
