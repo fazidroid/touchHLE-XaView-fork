296cf636cbb32cd39b83b64cfd73a21ef183c5b7
ciciplusplus <ciciplusplus@gmail.com>
2025-09-14 22:22:16 +0200

    Enable JPEG support for stb_image.h
    Change-Id: I2bc69950419897a65ba428df8ab8dfd833a0b141

--- src/image/stb_image_wrapper/lib.c
+++ src/image/stb_image_wrapper/lib.c
@@ -4,6 +4,7 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 #define STB_IMAGE_IMPLEMENTATION
+#define STBI_ONLY_JPEG
 #define STBI_ONLY_PNG
 #define STBI_NO_STDIO
 #include "../../../vendor/stb/stb_image.h"
