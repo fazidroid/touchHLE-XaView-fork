7fe4ddcf20583651b8522c077c651c0be9153c60
abnormalmaps <abnormalmaps@gmail.com>
2025-10-16 03:55:58 -0400

    Fix AutoReleasePoolTest licensing
    Change-Id: I1005cceac97d33cd253154cbf02abf253b097787

--- tests/TestApp_source/AutoReleasePoolTest.m
+++ tests/TestApp_source/AutoReleasePoolTest.m
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 #include "system_headers.h"
 static int dealloc_counter = 0;
 @implementation DeallocDetection : NSObject
