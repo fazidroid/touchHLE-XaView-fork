fazidroid/touchHLE-XaView-fork

commit b71b6d78b3ba2ea2f020f3ef5ed3e94c7d2dc136

Update plist to v1.8.0 and quick-xml to v0.38.4
In Metal Gear Solid Touch (v1.0.0), the plist crate caused a crash when
English was used, due to an InvalidTrailerObjectOffsetSize error when
reading the en.lproj/Localizable.strings file. It was worked around
by setting Japanese as the default language, since
ja.lproj/Localizable.strings loaded successfully.

The latest version of plist fixes this issue, and thus the workaround
is no longer required.

quick-xml, being both a direct dependency as well as transitive
dependency through plist, was also updated to avoid requiring two
versions of the same crate.

The commit updates the versions in Cargo.toml, removes the Japanese
language default setting, and replaces calls to the unescape method
in quick-xml with its replacement method decode.

Fixes GitHub issue #430.

Change-Id: I9b3dae3e994acc1df8016af69a4d2c250cf3f6ef

src/frameworks/foundation/ns_xml_parser.rs
@@ -149,7 +149,7 @@ pub const CLASSES: ClassExports = objc_classes! {
                 }
             }
             Event::Text(e) => {
-                let text = e.unescape().unwrap().into_owned();
+                let text = e.decode().unwrap().to_string();
                 // FIXME: skipping the end of the parsed string?
                 if text != "\0" {
                     let sel: SEL = env
@@ -214,7 +214,7 @@ pub const CLASSES: ClassExports = objc_classes! {
                         .register_host_selector("parser:foundCharacters:".to_string(), &mut env.mem);
                     let responds: bool = msg![env; delegate respondsToSelector:sel];
                     if responds {
-                        let text = e.escape().unwrap().unescape().unwrap().to_string();
+                        let text = e.escape().unwrap().decode().unwrap().to_string();
                         let text = from_rust_string(env, text);
                         let text = autorelease(env, text);
                         () = msg![env; delegate parser:this foundCharacters:text];
@@ -222,7 +222,7 @@ pub const CLASSES: ClassExports = objc_classes! {
                 }
             }
             Event::Comment(e) => {
-                let comment = e.unescape().unwrap().into_owned();
+                let comment = e.decode().unwrap().to_string();
                 let sel: SEL = env
                     .objc
                     .register_host_selector("parser:foundComment:".to_string(), &mut env.mem);

Cargo.toml
@@ -41,7 +41,7 @@ gl_generator = "0.14.0"
 caf = "0.1.0"
 hound = "3.5.0"
 mach_object = "0.1.17"
-plist = "1.3.1"
+plist = "1.8.0"
 zip = { version = "0.6.4", default-features = false, features = ["deflate"] }
 rusttype = "0.9.3"
 # While most Symphonia codecs are (likely) unpatented/have freely licenseable 
@@ -50,7 +50,7 @@ rusttype = "0.9.3"
 # be ok.
 # (The above statements should not understood as legal claims/advice.)
 symphonia = { version = "0.5.3", default-features = false, features = ["adpcm", "wav", "aac", "isomp4", "mp3", "alac"] }
-quick-xml = "0.36.2"
+quick-xml = "0.38.4"
 md5 = "0.7.0"
 yore = "1.1.0"
 encoding_rs = "0.8.35"

commit af3624ff7d659a68d10e99cd3da925be32432e6a

Fix XML parsing regression introduced by b71b6d7
Since commit b71b6d7, Cut The Rope would render garbled text everywhere.

In that commit, calls to quick-xml's unescape() function was replaced
with calls to decode(). This worked fine everywhere except in the CDATA
handling block, where the text would remain escaped, causing incorrect
text parsing in Cut The Rope.

Change-Id: I3ff5ff47390c257835d5a6d6b139a2a2bbfef26d

src/frameworks/foundation/ns_xml_parser.rs
@@ -214,7 +214,7 @@ pub const CLASSES: ClassExports = objc_classes! {
                         .register_host_selector("parser:foundCharacters:".to_string(), &mut env.mem);
                     let responds: bool = msg![env; delegate respondsToSelector:sel];
                     if responds {
-                        let text = e.escape().unwrap().decode().unwrap().to_string();
+                        let text = e.decode().unwrap().to_string();
                         let text = from_rust_string(env, text);
                         let text = autorelease(env, text);
                         () = msg![env; delegate parser:this foundCharacters:text];