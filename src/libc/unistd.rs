691353af460999f75a24c3edf71277b00f4b89dc
ciciplusplus <ciciplusplus@gmail.com>
2026-01-18 23:59:07 +0100

    Properly set most errors in access()
    Change-Id: Ic07160d1b385da10d319e4983dcb32ca56621a44

--- src/libc/unistd.rs
+++ src/libc/unistd.rs
@@ -7,7 +7,7 @@
 
 use crate::dyld::{export_c_func, FunctionExports};
 use crate::fs::GuestPath;
-use crate::libc::errno::set_errno;
+use crate::libc::errno::{set_errno, EACCES, ENOENT, EROFS};
 use crate::libc::posix_io::{FileDescriptor, STDERR_FILENO, STDIN_FILENO, STDOUT_FILENO};
 use crate::mem::{ConstPtr, GuestUSize, MutPtr, PAGE_SIZE};
 use crate::Environment;
@@ -74,13 +74,13 @@ fn access(env: &mut Environment, path: ConstPtr<u8>, mode: i32) -> i32 {
     let binding = env.mem.cstr_at_utf8(path).unwrap();
     let guest_path = GuestPath::new(&binding);
     let (exists, read, write, execute) = env.fs.access(guest_path);
-    // TODO: set errno
     // TODO: support ORing
     match mode {
         F_OK => {
             if exists {
                 0
             } else {
+                set_errno(env, ENOENT);
                 -1
             }
         }
@@ -88,6 +88,7 @@ fn access(env: &mut Environment, path: ConstPtr<u8>, mode: i32) -> i32 {
             if execute {
                 0
             } else {
+                set_errno(env, EACCES);
                 -1
             }
         }
@@ -95,6 +96,7 @@ fn access(env: &mut Environment, path: ConstPtr<u8>, mode: i32) -> i32 {
             if write {
                 0
             } else {
+                set_errno(env, EROFS);
                 -1
             }
         }
@@ -102,6 +104,8 @@ fn access(env: &mut Environment, path: ConstPtr<u8>, mode: i32) -> i32 {
             if read {
                 0
             } else {
+                // TODO: is it the correct error?
+                set_errno(env, EACCES);
                 -1
             }
         }
