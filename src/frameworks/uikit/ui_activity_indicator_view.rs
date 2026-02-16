c04700269376dca5b6fec7ee0fb382ab8d0cdb52
ciciplusplus <ciciplusplus@gmail.com>
2026-02-16 22:52:33 +0100

    Implement UIActivityIndicatorView isAnimating property getter
    Change-Id: I4615df8aa00047f2a7451c288cc4ca060139b0a2

--- src/frameworks/uikit/ui_activity_indicator_view.rs
+++ src/frameworks/uikit/ui_activity_indicator_view.rs
@@ -6,15 +6,32 @@
 //! `UIActivityIndicatorView`.
 
 use crate::frameworks::foundation::NSInteger;
-use crate::objc::{id, msg, objc_classes, todo_objc_setter, ClassExports};
+use crate::objc::{
+    id, impl_HostObject_with_superclass, msg, objc_classes, todo_objc_setter, ClassExports,
+    NSZonePtr,
+};
 
 type UIActivityIndicatorViewStyle = NSInteger;
+
+pub struct UIActivityIndicatorViewHostObject {
+    superclass: super::ui_view::UIViewHostObject,
+    animating: bool,
+}
+impl_HostObject_with_superclass!(UIActivityIndicatorViewHostObject);
 
 pub const CLASSES: ClassExports = objc_classes! {
 
 (env, this, _cmd);
 
 @implementation UIActivityIndicatorView: UIView
+
++ (id)allocWithZone:(NSZonePtr)_zone {
+    let host_object = Box::new(UIActivityIndicatorViewHostObject {
+        superclass: Default::default(),
+        animating: false,
+    });
+    env.objc.alloc_object(this, host_object, &mut env.mem)
+}
 
 - (id)initWithActivityIndicatorStyle:(UIActivityIndicatorViewStyle)_style {
     // TODO: proper init
@@ -27,9 +44,15 @@ pub const CLASSES: ClassExports = objc_classes! {
 
 - (())startAnimating {
     log!("TODO: [(UIActivityIndicatorView *){:?} startAnimating]", this);
+    env.objc.borrow_mut::<UIActivityIndicatorViewHostObject>(this).animating = true;
 }
 - (())stopAnimating {
     log!("TODO: [(UIActivityIndicatorView *){:?} stopAnimating]", this);
+    env.objc.borrow_mut::<UIActivityIndicatorViewHostObject>(this).animating = false;
+}
+
+- (bool)isAnimating {
+    env.objc.borrow::<UIActivityIndicatorViewHostObject>(this).animating
 }
 
 - (())setHidesWhenStopped:(bool)_hides {
