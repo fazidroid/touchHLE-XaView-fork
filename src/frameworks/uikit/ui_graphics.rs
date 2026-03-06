7935ebce9835bee5d12a8dce266a768e1f07adf5
XaView <79099247+XaViewDnK@users.noreply.github.com>
2026-03-07 00:46:37 +0300

    button

--- src/frameworks/uikit/ui_graphics.rs
+++ src/frameworks/uikit/ui_graphics.rs
@@ -7,10 +7,13 @@
 
 use crate::dyld::{export_c_func, FunctionExports};
 use crate::frameworks::core_graphics::cg_context::{
-    CGContextRef, CGContextRelease, CGContextRetain,
+    CGContextRef, CGContextRelease, CGContextRetain, CGContextHostObject, CGContextSubclass
 };
+use crate::frameworks::core_graphics::cg_bitmap_context::CGBitmapContextData;
+use crate::frameworks::core_graphics::cg_affine_transform::CGAffineTransform;
 use crate::frameworks::core_graphics::CGSize;
-use crate::objc::{id, nil};
+use crate::objc::{id, nil, msg_class};
+use crate::mem::Ptr;
 use crate::Environment;
 
 #[derive(Default)]
@@ -27,8 +30,9 @@ pub fn UIGraphicsPushContext(env: &mut Environment, context: CGContextRef) {
         .push(context);
 }
 pub fn UIGraphicsPopContext(env: &mut Environment) {
-    let context = env.framework_state.uikit.ui_graphics.context_stack.pop();
-    CGContextRelease(env, context.unwrap());
+    if let Some(context) = env.framework_state.uikit.ui_graphics.context_stack.pop() {
+        CGContextRelease(env, context);
+    }
 }
 pub fn UIGraphicsGetCurrentContext(env: &mut Environment) -> CGContextRef {
     env.framework_state
@@ -40,17 +44,40 @@ pub fn UIGraphicsGetCurrentContext(env: &mut Environment) -> CGContextRef {
         .unwrap_or(nil)
 }
 
-pub fn UIGraphicsBeginImageContext(_env: &mut Environment, _size: CGSize) {
-    log!("TODO: UIGraphicsBeginImageContext");
+pub fn UIGraphicsBeginImageContext(env: &mut Environment, size: CGSize) {
+    log!("TODO: UIGraphicsBeginImageContext size {:?}", size);
+    
+    let class = env.objc.get_known_class("_touchHLE_CGContext", &mut env.mem);
+    let host_object = Box::new(CGContextHostObject {
+        subclass: CGContextSubclass::CGBitmapContext(CGBitmapContextData {
+            data: Ptr::null(),
+            data_is_owned: false,
+            width: size.width as usize,
+            height: size.height as usize,
+            bits_per_component: 8,
+            bytes_per_row: (size.width * 4.0) as usize,
+            color_space: nil,
+            bitmap_info: 0,
+        }),
+        rgb_fill_color: (0.0, 0.0, 0.0, 0.0),
+        transform: CGAffineTransform::identity(),
+        state_stack: Vec::new(),
+    });
+    
+    let context = env.objc.alloc_object(class, host_object, &mut env.mem);
+    UIGraphicsPushContext(env, context);
+    // Remove extra +1 retain count from alloc, so it dies properly on pop
+    CGContextRelease(env, context);
 }
 
 pub fn UIGraphicsGetImageFromCurrentImageContext(env: &mut Environment) -> id {
     log!("TODO: UIGraphicsGetImageFromCurrentImageContext");
-    crate::objc::msg_class![env; UIImage alloc]
+    msg_class![env; UIImage alloc]
 }
 
-pub fn UIGraphicsEndImageContext(_env: &mut Environment) {
+pub fn UIGraphicsEndImageContext(env: &mut Environment) {
     log!("TODO: UIGraphicsEndImageContext");
+    UIGraphicsPopContext(env);
 }
 
 pub const FUNCTIONS: FunctionExports = &[
