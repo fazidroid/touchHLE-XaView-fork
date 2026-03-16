fee9dd893818291d8622b099e618f1643cdff13d
XaView <79099247+XaViewDnK@users.noreply.github.com>
2026-03-16 19:20:24 +0300

    A7_1.0.9

--- src/objc/messages.rs
+++ src/objc/messages.rs
@@ -169,6 +169,11 @@ Type mismatch when sending message {} to {:?}!
                 env.cpu.regs_mut()[0..2].fill(0);
                 return;
             }
+            // FakeAdManager
+            if name == "ASIdentifierManager" {
+                env.cpu.regs_mut()[0..2].fill(0);
+                return;
+            }
             panic!(
                 "Class \"{}\" ({:?}) is unimplemented. Call to {} method \"{}\".",
                 name,
