d773e51dffd70610f99fb532e1862ed7bdba3e4a
XaView <79099247+XaViewDnK@users.noreply.github.com>
2026-03-16 14:27:27 +0300

    A7_1.1.0

--- src/libc/cxxabi.rs
+++ src/libc/cxxabi.rs
@@ -15,26 +15,29 @@ use crate::Environment;
 
 fn __cxa_atexit(
     _env: &mut Environment,
-    func: GuestFunction, // void (*func)(void *)
-    p: MutVoidPtr,
-    d: MutVoidPtr,
+    _func: GuestFunction,
+    _p: MutVoidPtr,
+    _d: MutVoidPtr,
 ) -> i32 {
-    // TODO: when this is implemented, make sure it's properly compatible with
-    // C atexit.
-    log!(
-        "TODO: __cxa_atexit({:?}, {:?}, {:?}) (unimplemented)",
-        func,
-        p,
-        d
-    );
-    0 // success
+    // BypassCxaAtexit
+    0
 }
 
-fn __cxa_finalize(_env: &mut Environment, d: MutVoidPtr) {
-    log!("TODO: __cxa_finalize({:?}) (unimplemented)", d);
+fn __cxa_finalize(_env: &mut Environment, _d: MutVoidPtr) {
+    // BypassCxaFinalize
+}
+
+fn _Unwind_SjLj_Register(_env: &mut Environment, _context: MutVoidPtr) {
+    // BypassSjLjRegister
+}
+
+fn _Unwind_SjLj_Unregister(_env: &mut Environment, _context: MutVoidPtr) {
+    // BypassSjLjUnregister
 }
 
 pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(__cxa_atexit(_, _, _)),
     export_c_func!(__cxa_finalize(_)),
+    export_c_func!(_Unwind_SjLj_Register(_)),
+    export_c_func!(_Unwind_SjLj_Unregister(_)),
 ];
