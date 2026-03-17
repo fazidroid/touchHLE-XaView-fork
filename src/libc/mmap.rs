09529773c957c8f3711790dfa8f57fc6059665ec
ciciplusplus <ciciplusplus@gmail.com>
2026-03-17 22:31:06 +0100

    Implement anonymously mapped memory for mmap() MAP_ANON
    Change-Id: Ia1e8a9343039dd44e94fd88b66563e44abf913b0

--- src/libc/mmap.rs
+++ src/libc/mmap.rs
@@ -10,7 +10,7 @@ use crate::export_c_func;
 use crate::libc::errno::{set_errno, EINVAL};
 use crate::libc::posix_io;
 use crate::libc::posix_io::{off_t, FileDescriptor, SEEK_SET};
-use crate::mem::{GuestUSize, MutVoidPtr};
+use crate::mem::{GuestUSize, MutVoidPtr, PAGE_SIZE_ALIGN_MASK};
 use std::collections::HashMap;
 
 #[allow(dead_code)]
@@ -23,13 +23,13 @@ pub struct State {
     allocations: HashMap<MutVoidPtr, GuestUSize>,
 }
 
-/// Our implementation of mmap is really simple: it's just load entirety of
-/// file in memory!
+/// For files, our implementation of mmap is really simple:
+/// it's just load entirety of file in memory!
 fn mmap(
     env: &mut Environment,
     addr: MutVoidPtr,
     len: GuestUSize,
-    _prot: i32,
+    prot: i32,
     flags: i32,
     fd: FileDescriptor,
     offset: off_t,
@@ -37,20 +37,42 @@ fn mmap(
     // TODO: handle errno properly
     set_errno(env, 0);
 
-    log_dbg!("mmap len {}", len);
+    log_dbg!(
+        "mmap({:?}, {}, {}, {}, {}, {})",
+        addr,
+        len,
+        prot,
+        flags,
+        fd,
+        offset
+    );
 
-    assert!(addr.is_null());
     assert_eq!(offset, 0);
-    assert_eq!((flags & MAP_ANON), 0);
-    let new_offset = posix_io::lseek(env, fd, offset, SEEK_SET);
-    assert_eq!(new_offset, offset);
-    let ptr = env.mem.alloc(len);
+    // TODO: use vm_allocate() instead
+    let ptr = env.mem.calloc(len);
+
+    if (flags & MAP_ANON) != 0 {
+        assert!(ptr.to_bits() & PAGE_SIZE_ALIGN_MASK == 0);
+        assert_eq!(fd, -1);
+        if !addr.is_null() {
+            log!(
+                "Warning: mmap MAP_ANON ignoring hint for address {:?}, actual is {:?}",
+                addr,
+                ptr
+            );
+        }
+    } else {
+        assert!(addr.is_null());
+        let new_offset = posix_io::lseek(env, fd, offset, SEEK_SET);
+        assert_eq!(new_offset, offset);
+
+        let read = posix_io::read(env, fd, ptr, len);
+        assert_eq!(read as u32, len);
+    };
 
     assert!(!env.libc_state.mmap.allocations.contains_key(&ptr));
     env.libc_state.mmap.allocations.insert(ptr, len);
 
-    let read = posix_io::read(env, fd, ptr, len);
-    assert_eq!(read as u32, len);
     ptr
 }
 
@@ -58,7 +80,7 @@ fn munmap(env: &mut Environment, addr: MutVoidPtr, len: GuestUSize) -> i32 {
     // TODO: handle errno properly
     set_errno(env, 0);
 
-    log_dbg!("munmap len {}", len);
+    log_dbg!("munmap({:?}, {})", addr, len);
 
     if len == 0 {
         set_errno(env, EINVAL);
