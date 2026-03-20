touchHLE/touchHLE

Switch to coroutine based threading system
Switch the threading system such that each guest thread has one
corresponding host (stackful) coroutine.

Change-Id: I2ea2380b4fb88bf7f3fb6d3d8ac688468a8ad7e8

src/frameworks/audio_toolbox/audio_unit.rs
@@ -177,7 +177,7 @@ fn AudioOutputUnitStart(env: &mut Environment, ci: AudioUnit) -> OSStatus {
     let context = env
         .framework_state
         .audio_toolbox
-        .make_al_context_current(&mut env.audio);
+        .make_al_context_current(env.audio.as_mut());
 
     let mut source: ALuint = 0;
     unsafe {
@@ -204,7 +204,7 @@ fn AudioOutputUnitStop(env: &mut Environment, ci: AudioUnit) -> OSStatus {
     let at_state = &mut env.framework_state.audio_toolbox;
     let context = at_state
         .al_context
-        .make_al_context_current(&mut env.audio);
+        .make_al_context_current(env.audio.as_mut());
 
     let audio_components_state = &mut at_state.audio_components;
 
@@ -240,7 +240,7 @@ pub fn render_audio_unit(env: &mut Environment, audio_unit: AudioUnit) {
     let at_state = &mut env.framework_state.audio_toolbox;
     let context = at_state
         .al_context
-        .make_al_context_current(&mut env.audio);
+        .make_al_context_current(env.audio.as_mut());
 
     let audio_session::State {
         current_hardware_sample_rate,
@@ -380,7 +380,7 @@ pub fn render_audio_unit(env: &mut Environment, audio_unit: AudioUnit) {
     let at_state = &mut env.framework_state.audio_toolbox;
     let context = at_state
         .al_context
-        .make_al_context_current(&mut env.audio);
+        .make_al_context_current(env.audio.as_mut());
 
     let (al_format, _sample_rate, processed_data) =
         decode_buffer(&env.mem, &stream_format, buffer1Data.cast(), buffer_size);

fazidroid/touchHLE-XaView-fork

Change-Id: I9488116c9b9904beba3e47169fbc60b2c02522a2

src/frameworks/audio_toolbox/audio_unit.rs
@@ -201,7 +201,7 @@ fn AudioOutputUnitStart(env: &mut Environment, ci: AudioUnit) -> OSStatus {
     let context = env
         .framework_state
         .audio_toolbox
-        .make_al_context_current(&mut env.openal_manager);
+        .make_al_context_current(env.openal_manager.as_mut());
 
     let mut source: ALuint = 0;
     unsafe {
@@ -228,7 +228,7 @@ fn AudioOutputUnitStop(env: &mut Environment, ci: AudioUnit) -> OSStatus {
     let at_state = &mut env.framework_state.audio_toolbox;
     let context = at_state
         .al_context
-        .make_al_context_current(&mut env.openal_manager);
+        .make_al_context_current(env.openal_manager.as_mut());
 
     let audio_components_state = &mut at_state.audio_components;
 
@@ -264,7 +264,7 @@ pub fn render_audio_unit(env: &mut Environment, audio_unit: AudioUnit) {
     let at_state = &mut env.framework_state.audio_toolbox;
     let context = at_state
         .al_context
-        .make_al_context_current(&mut env.openal_manager);
+        .make_al_context_current(env.openal_manager.as_mut());
 
     let audio_session::State {
         current_hardware_sample_rate,
@@ -413,7 +413,7 @@ pub fn render_audio_unit(env: &mut Environment, audio_unit: AudioUnit) {
     let at_state = &mut env.framework_state.audio_toolbox;
     let context = at_state
         .al_context
-        .make_al_context_current(&mut env.openal_manager);
+        .make_al_context_current(env.openal_manager.as_mut());
 
     let (al_format, _sample_rate, processed_data) =
         decode_buffer(&env.mem, &stream_format, buffer1Data.cast(), buffer_size);