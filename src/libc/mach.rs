9305124fa9d6f1187719053f75965471d05eaec0
ciciplusplus <ciciplusplus@gmail.com>
2026-03-28 11:25:47 +0100

    Add useful resources for mach module documentation.
    Change-Id: I52e6c8cb090b079862187fc2282fe9bf8a8274f3

--- src/libc/mach.rs
+++ src/libc/mach.rs
@@ -4,6 +4,16 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 //! `mach`
+//!
+//! Resources:
+//! - [The GNU Mach Reference Manual](https://www.gnu.org/software/hurd/gnumach-doc/mach.pdf).
+//!   While being `the GNU Mach microkernel` manual, it serves as a good
+//!   general reference doc explaining various undocumented Mach
+//!   interfaces.
+//! - MIT's [Mach IPC Interface](https://web.mit.edu/darwin/src/modules/xnu/osfmk/man/).
+//!   Another online manual with short description for each function.
+//!   The main downside is that argument names are not following Apple's
+//!   conventions, so you should double-check the real definitions.
 
 #![allow(non_camel_case_types)]
 
