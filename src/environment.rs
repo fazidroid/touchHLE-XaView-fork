3e809496ea8310c1121e8f78ede9602d51c76123
XaView <79099247+XaViewDnK@users.noreply.github.com>
2026-04-03 13:59:40 +0300

    bad

--- src/environment.rs
+++ src/environment.rs
@@ -1640,6 +1640,7 @@ impl Environment {
 
         // SetupHeartbeatTimer
         let mut last_heartbeat = Instant::now();
+        let mut bg_hang_counters = [0u32; 16];
 
         loop {
             while self
@@ -1678,20 +1679,25 @@ impl Environment {
                     self.cpu.branch(GuestFunction::from_addr_with_thumb_bit(target_lr));
                 }
 
-                // TargetedHangBypass
-                let lr = self.cpu.regs()[cpu::Cpu::LR];
-                if (pc == 0x00c3375c && lr == 0x00afdaf9) ||
-                   (pc == 0x00c3376c && lr == 0x00a16403) ||
-                   ((pc == 0x00a8b93e || pc == 0x00a8b978) && lr == 0x00a81fe1) 
-                {
-                    echo!("WARNING: Safely unwinding targeted hang at PC {:#010x}, LR {:#010x}!", pc, lr);
-                    let fp0 = self.cpu.regs()[7];
-                    let prev_fp: u32 = self.mem.read(mem::ConstPtr::<u32>::from_bits(fp0));
-                    let target_lr: u32 = self.mem.read(mem::ConstPtr::<u32>::from_bits(fp0 + 4));
-                    self.cpu.regs_mut()[7] = prev_fp;
-                    self.cpu.regs_mut()[cpu::Cpu::SP] = fp0 + 8;
-                    self.cpu.regs_mut()[0] = 0;
-                    self.cpu.branch(GuestFunction::from_addr_with_thumb_bit(target_lr));
+                // SmartNetworkHangBypass
+                let tid = self.current_thread;
+                if (pc == 0x00c3375c || pc == 0x00c3376c) && tid != 0 && tid < 16 {
+                    bg_hang_counters[tid] = bg_hang_counters[tid].wrapping_add(1);
+                    if bg_hang_counters[tid] > 10_000_000 {
+                        echo!("WARNING: Safely unwinding deep background network hang at {:#010x} for Thread {}!", pc, tid);
+                        let fp0 = self.cpu.regs()[7];
+                        let prev_fp: u32 = self.mem.read(mem::ConstPtr::<u32>::from_bits(fp0));
+                        let target_lr: u32 = self.mem.read(mem::ConstPtr::<u32>::from_bits(fp0 + 4));
+                        self.cpu.regs_mut()[7] = prev_fp;
+                        self.cpu.regs_mut()[cpu::Cpu::SP] = fp0 + 8;
+                        self.cpu.regs_mut()[0] = 0;
+                        self.cpu.branch(GuestFunction::from_addr_with_thumb_bit(target_lr));
+                        bg_hang_counters[tid] = 0;
+                    }
+                } else if tid != 0 && tid < 16 {
+                    if pc < 0x00c33700 || pc > 0x00c33800 {
+                        bg_hang_counters[tid] = 0;
+                    }
                 }
 
                 // PrintDebugHeartbeat
