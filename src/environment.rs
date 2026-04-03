84c13ff39d676f1688bf40734719b1a7ebf0e167
XaView <79099247+XaViewDnK@users.noreply.github.com>
2026-04-03 22:13:44 +0300

    mankind

--- src/environment.rs
+++ src/environment.rs
@@ -1700,14 +1700,13 @@ impl Environment {
                     self.cpu.regs_mut()[0] = 0;
                     self.cpu.branch(GuestFunction::from_addr_with_thumb_bit(target_lr));
                 } else if (pc == 0x0020a9c2 || pc == 0x0020a9c8) && self.current_thread == 0 {
-                    // DeepBypassSplash
-                    echo!("WARNING: Deep unwinding splash deadlock at {:#010x}! Thread: {}", pc, self.current_thread);
+                    // BypassSplashDeadlock
+                    echo!("WARNING: Safely unwinding splash deadlock at {:#010x}! Thread: {}", pc, self.current_thread);
                     let fp0 = self.cpu.regs()[7];
-                    let fp1: u32 = self.mem.read(mem::ConstPtr::<u32>::from_bits(fp0));
-                    let prev_fp: u32 = self.mem.read(mem::ConstPtr::<u32>::from_bits(fp1));
-                    let target_lr: u32 = self.mem.read(mem::ConstPtr::<u32>::from_bits(fp1 + 4));
+                    let prev_fp: u32 = self.mem.read(mem::ConstPtr::<u32>::from_bits(fp0));
+                    let target_lr: u32 = self.mem.read(mem::ConstPtr::<u32>::from_bits(fp0 + 4));
                     self.cpu.regs_mut()[7] = prev_fp;
-                    self.cpu.regs_mut()[cpu::Cpu::SP] = fp1 + 8;
+                    self.cpu.regs_mut()[cpu::Cpu::SP] = fp0 + 8;
                     self.cpu.regs_mut()[0] = 0;
                     self.cpu.branch(GuestFunction::from_addr_with_thumb_bit(target_lr));
                 }
