2c135e99cc3863d0680f55cf69275a1dff9d0f76
fazidroid <84150589+fazidroid@users.noreply.github.com>
2026-04-05 00:24:19 +0530

    GT free+ 1.3.4 freezing fix

--- src/frameworks/system_configuration/sc_network_reachability.rs
+++ src/frameworks/system_configuration/sc_network_reachability.rs
@@ -11,12 +11,13 @@ use crate::frameworks::core_foundation::cf_allocator::{kCFAllocatorDefault, CFAl
 use crate::frameworks::core_foundation::CFTypeRef;
 use crate::libc::sys::socket::sockaddr;
 use crate::mem::{ConstPtr, MutPtr, MutVoidPtr, Ptr};
-use crate::objc::{msg, objc_classes, Class, ClassExports, HostObject};
+use crate::objc::{objc_classesExports, HostObject};
 use crate::Environment;
 use std::net::SocketAddrV4;
 
 type SCNetworkReachabilityFlags = u32;
 const kSCNetworkReachabilityFlagsReachable: SCNetworkReachabilityFlags = 1 << 1;
+#[allow(dead_code)]
 #[allow(dead_code)]
 const kSCNetworkReachabilityFlagsIsDirect: SCNetworkReachabilityFlags = 1 << 17;
 
@@ -32,6 +33,7 @@ pub const CLASSES: ClassExports = objc_classes! {
 
 };
 
+#[allow(dead_code)]
 #[allow(dead_code)]
 struct SCNetworkReachabilityHostObject {
     address: Option<SocketAddrV4>,
@@ -159,7 +161,7 @@ fn SCNetworkReachabilitySetCallback(
             .get_known_class("_touchHLE_SCNetworkReachability", &mut env.mem)
     );
     log!(
-        "TODO: SCNetworkReachabilitySetCallback({:?}, {:?}, {:?}) -> FALSE",
+        "TODO: SCNetworkReachabilitySetCallback({:?}, {:?}, {:?}) -> TRUE",
         target,
         callout,
         context
