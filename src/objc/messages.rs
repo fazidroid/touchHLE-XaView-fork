72aa28ee3cc6119e29aacc88f99580f04aa511f7
fazidroid <84150589+fazidroid@users.noreply.github.com>
2026-04-05 16:06:23 +0530

    GT racing MA WIP

--- src/objc/messages.rs
+++ src/objc/messages.rs
@@ -53,7 +53,7 @@ fn objc_msgSend_inner(
         || sel_str == "isSecureTextEntry"
         || sel_str == "copyWithZone:"
     {
-        env.objc.objects.retain(receiver);
+        env.objc.retain_object(receiver);
         env.cpu.regs_mut()[0] = receiver.to_bits();
         return;
     }
