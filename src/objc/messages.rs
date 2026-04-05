8015c769804c95b049000916feb0dea0bb0b915c
XaView <79099247+XaViewDnK@users.noreply.github.com>
2026-04-05 10:22:01 +0300

    timezone

--- src/objc/messages.rs
+++ src/objc/messages.rs
@@ -106,6 +106,11 @@ fn objc_msgSend_inner(
                 env.cpu.regs_mut()[0..2].fill(0);
                 return;
             }
+            // BypassTimeZone
+            if selector.as_str(&env.mem) == "defaultTimeZone" {
+                env.cpu.regs_mut()[0..2].fill(0);
+                return;
+            }
 
             panic!(
                 "{} {:?} ({}class \"{}\", {:?}){} does not respond to selector \"{}\"!",
