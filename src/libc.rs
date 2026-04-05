bc5068f751fa3926fa3b08ac758c912118efa2e8
fazidroid <84150589+fazidroid@users.noreply.github.com>
2026-04-06 00:49:08 +0530

    GT racing MA WIP

--- src/libc.rs
+++ src/libc.rs
@@ -83,7 +83,7 @@ pub const DYLIB: crate::dyld::HostDylib = crate::dyld::HostDylib {
         pthread::mutex::FUNCTIONS,
         pthread::once::FUNCTIONS,
         pthread::thread::FUNCTIONS,
-        pthread::rwlock::FUNCTIONS, // <--- THE NEW RWLOCK REGISTRY
+        pthread::rwlock::FUNCTIONS,
         sched::FUNCTIONS,
         semaphore::FUNCTIONS,
         setjmp::FUNCTIONS,
