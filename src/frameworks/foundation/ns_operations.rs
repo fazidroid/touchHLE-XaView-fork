9b83bf894f091b55f2fcc5d8fc72973a1bc2b24f
fazidroid <84150589+fazidroid@users.noreply.github.com>
2026-04-06 14:02:40 +0530

    GT Free+ WIP

---
+++ src/frameworks/foundation/ns_operations.rs
@@ -0,0 +1,95 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+//! `NSOperation` and `NSOperationQueue`.
+
+use crate::objc::{id, msg, nil, objc_classes, SEL, ClassExports, HostObject};
+use crate::Environment;
+
+pub(super) struct NSInvocationOperationHostObject {
+    pub target: id,
+    pub sel: SEL,
+    pub arg: id,
+}
+impl HostObject for NSInvocationOperationHostObject {}
+
+pub const CLASSES: ClassExports = objc_classes! {
+
+(env, this, _cmd);
+
+@implementation NSOperationQueue: NSObject
+
++ (id)alloc {
+    env.objc.alloc_object(this, Box::new(()), &mut env.mem)
+}
+
+- (id)init { this }
+
+- (())addOperation:(id)op {
+    log_dbg!("[(NSOperationQueue*){:?} addOperation:{:?}] (executing synchronously!)", this, op);
+    () = msg![env; op start];
+}
+
+- (())setMaxConcurrentOperationCount:(isize)_count { }
+
+@end
+
+
+@implementation NSOperation: NSObject
+
++ (id)alloc {
+    env.objc.alloc_object(this, Box::new(()), &mut env.mem)
+}
+
+- (id)init { this }
+
+- (())start {
+    () = msg![env; this main];
+}
+
+- (())main { }
+
+- (bool)isCancelled { false }
+
+- (())cancel { }
+
+@end
+
+
+@implementation NSInvocationOperation: NSOperation
+
++ (id)alloc {
+    let host_object = Box::new(NSInvocationOperationHostObject {
+        target: nil,
+        sel: SEL::from_raw(0),
+        arg: nil,
+    });
+    env.objc.alloc_object(this, host_object, &mut env.mem)
+}
+
+- (id)initWithTarget:(id)target selector:(SEL)sel object:(id)arg {
+    let host_object = env.objc.borrow_mut::<NSInvocationOperationHostObject>(this);
+    host_object.target = target;
+    host_object.sel = sel;
+    host_object.arg = arg;
+    this
+}
+
+- (())main {
+    let host_object = env.objc.borrow::<NSInvocationOperationHostObject>(this);
+    let target = host_object.target;
+    let sel = host_object.sel;
+    let arg = host_object.arg;
+    
+    if arg != nil {
+        () = msg![env; target performSelector:sel withObject:arg];
+    } else {
+        () = msg![env; target performSelector:sel];
+    }
+}
+
+@end
+
+};
