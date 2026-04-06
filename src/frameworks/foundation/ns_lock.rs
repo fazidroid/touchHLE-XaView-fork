fcff135cafaecd6e46d10ccf391c9d35e71f928e
fazidroid <84150589+fazidroid@users.noreply.github.com>
2026-04-07 00:05:48 +0530

    GT Free+ 1.3.4 WIP

--- src/frameworks/foundation/ns_lock.rs
+++ src/frameworks/foundation/ns_lock.rs
@@ -7,7 +7,6 @@
 
 use crate::environment::MutexType::PTHREAD_MUTEX_RECURSIVE;
 use crate::environment::{MutexId, PTHREAD_MUTEX_DEFAULT};
-use crate::msg;
 use crate::objc::{id, nil, objc_classes, ClassExports, HostObject};
 
 struct NSLockHostObject {
@@ -23,48 +22,31 @@ pub const CLASSES: ClassExports = objc_classes! {
 @implementation NSLock: NSObject
 
 + (id)alloc {
-    log_dbg!("[NSLock alloc]");
     let mutex_id = env.mutex_state.init_mutex(PTHREAD_MUTEX_DEFAULT);
     let host_object = NSLockHostObject { mutex_id, name: nil };
     env.objc.alloc_object(this, Box::new(host_object), &mut env.mem)
 }
 
-// NSLocking protocol implementation
 - (())lock {
-    log_dbg!("[(NSLock *){:?} lock]", this);
     let host_object = env.objc.borrow::<NSLockHostObject>(this);
-    // Blocking lock is fine for the standard .lock call
-    if let Err(e) = env.lock_mutex(host_object.mutex_id) {
-        log!("Warning: Failed to acquire NSLock: {:?}", e);
-    }
+    let _ = env.lock_mutex(host_object.mutex_id);
 }
 
 - (())unlock {
-    log_dbg!("[(NSLock *){:?} unlock]", this);
     let host_object = env.objc.borrow::<NSLockHostObject>(this);
-    if !env.mutex_state.mutex_is_locked(host_object.mutex_id) {
-        log!("Warning: *** -[NSLock unlock]: lock (<NSLock: {:?}>) unlocked when not locked", this);
-    }
     let _ = env.unlock_mutex(host_object.mutex_id);
 }
 
 - (bool)tryLock {
     let host_object = env.objc.borrow::<NSLockHostObject>(this);
-    
-    // Non-blocking check
     if env.mutex_state.mutex_is_locked(host_object.mutex_id) {
         return false;
     }
-
-    match env.lock_mutex(host_object.mutex_id) {
-        Ok(_) => true,
-        Err(_) => false,
-    }
+    env.lock_mutex(host_object.mutex_id).is_ok()
 }
 
 - (())setName:(id)name {
-    let mut host_object = env.objc.borrow_mut::<NSLockHostObject>(this);
-    host_object.name = name;
+    env.objc.borrow_mut::<NSLockHostObject>(this).name = name;
 }
 
 - (id)name {
@@ -77,44 +59,31 @@ pub const CLASSES: ClassExports = objc_classes! {
 @implementation NSRecursiveLock: NSObject
 
 + (id)alloc {
-    log_dbg!("[NSRecursiveLock alloc]");
     let mutex_id = env.mutex_state.init_mutex(PTHREAD_MUTEX_RECURSIVE);
     let host_object = NSLockHostObject { mutex_id, name: nil };
     env.objc.alloc_object(this, Box::new(host_object), &mut env.mem)
 }
 
-// NSLocking protocol implementation
 - (())lock {
-    log_dbg!("[(NSRecursiveLock *){:?} lock]", this);
     let host_object = env.objc.borrow::<NSLockHostObject>(this);
     let _ = env.lock_mutex(host_object.mutex_id);
 }
 
 - (())unlock {
-    log_dbg!("[(NSRecursiveLock *){:?} unlock]", this);
     let host_object = env.objc.borrow::<NSLockHostObject>(this);
-    if !env.mutex_state.mutex_is_locked(host_object.mutex_id) {
-        log!("Warning: *** -[NSRecursiveLock unlock]: lock (<NSRecursiveLock: {:?}>) unlocked when not locked", this);
-    }
     let _ = env.unlock_mutex(host_object.mutex_id);
 }
 
 - (bool)tryLock {
     let host_object = env.objc.borrow::<NSLockHostObject>(this);
-    
-    // NSRecursiveLocks allow the same thread to lock multiple times.
-    // However, touchHLE's current lock_mutex might block if logic isn't perfect.
-    // We return false if locked to be safe against deadlocks in Gameloft games.
     if env.mutex_state.mutex_is_locked(host_object.mutex_id) {
         return false; 
     }
-    
     env.lock_mutex(host_object.mutex_id).is_ok()
 }
 
 - (())setName:(id)name {
-    let mut host_object = env.objc.borrow_mut::<NSLockHostObject>(this);
-    host_object.name = name;
+    env.objc.borrow_mut::<NSLockHostObject>(this).name = name;
 }
 
 - (id)name {
