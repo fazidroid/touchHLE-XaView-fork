920fa849e9b9f2076c36777c8833c01aac1eb422
fazidroid <84150589+fazidroid@users.noreply.github.com>
2026-04-07 00:03:20 +0530

    Restore cond.rs

--- src/libc/pthread/cond.rs
+++ src/libc/pthread/cond.rs
@@ -7,12 +7,12 @@
 
 use super::mutex::pthread_mutex_t;
 use crate::dyld::FunctionExports;
-use crate::libc::pthread::mutex::{pthread_mutex_lock, pthread_mutex_unlock};
+use crate::libc::pthread::mutex::pthread_mutex_unlock;
 use crate::mem::{ConstPtr, MutPtr, SafeRead};
 use crate::{export_c_func, Environment};
 use std::collections::{HashMap, VecDeque};
 
-use crate::environment::{ThreadBlock, ThreadId};
+use crate::environment::{MutexId, ThreadBlock, ThreadId};
 
 #[repr(C, packed)]
 pub struct pthread_condattr_t {}
@@ -42,80 +42,109 @@ impl State {
 pub struct CondHostObject {
     waiting: VecDeque<ThreadId>,
     pub(crate) waking: VecDeque<ThreadId>,
+    pub(crate) curr_mutex: Option<MutexId>,
 }
-
-pub const FUNCTIONS: FunctionExports = &[
-    export_c_func!(pthread_cond_init(_, _, _)),
-    export_c_func!(pthread_cond_signal(_, _)),
-    export_c_func!(pthread_cond_broadcast(_, _)),
-    export_c_func!(pthread_cond_destroy(_, _)),
-    export_c_func!(pthread_cond_wait(_, _, _)),
-    export_c_func!(pthread_cond_timedwait(_, _, _, _)),
-];
 
 pub fn pthread_cond_init(
     env: &mut Environment,
     cond: MutPtr<pthread_cond_t>,
-    _attr: ConstPtr<pthread_condattr_t>,
+    attr: ConstPtr<pthread_condattr_t>,
 ) -> i32 {
-    let cond_var = env.mem.alloc_and_write(OpaqueCond { _unused: 0 }).cast();
-    env.mem.write(cond, cond_var);
+    assert!(attr.is_null());
+    let opaque = env.mem.alloc_and_write(OpaqueCond { _unused: 0 });
+    env.mem.write(cond, opaque);
+
+    assert!(!State::get(env).condition_variables.contains_key(&opaque));
     State::get_mut(env).condition_variables.insert(
-        cond_var,
+        opaque,
         CondHostObject {
             waiting: VecDeque::new(),
             waking: VecDeque::new(),
+            curr_mutex: None,
         },
     );
-    0
-}
-
-pub fn pthread_cond_signal(env: &mut Environment, cond: MutPtr<pthread_cond_t>) -> i32 {
-    let cond_var = env.mem.read(cond);
-    if let Some(host_object) = State::get_mut(env).condition_variables.get_mut(&cond_var) {
-        if let Some(thread) = host_object.waiting.pop_front() {
-            host_object.waking.push_back(thread);
-        }
-    }
-    0
-}
-
-pub fn pthread_cond_broadcast(env: &mut Environment, cond: MutPtr<pthread_cond_t>) -> i32 {
-    let cond_var = env.mem.read(cond);
-    if let Some(host_object) = State::get_mut(env).condition_variables.get_mut(&cond_var) {
-        host_object.waking.extend(host_object.waiting.drain(..));
-    }
-    0
-}
-
-pub fn pthread_cond_destroy(env: &mut Environment, cond: MutPtr<pthread_cond_t>) -> i32 {
-    let cond_var = env.mem.read(cond);
-    State::get_mut(env).condition_variables.remove(&cond_var);
-    let _ = env.mem.free(cond_var.cast());
-    0
+    0 // success
 }
 
 pub fn pthread_cond_wait(
     env: &mut Environment,
-    _cond: MutPtr<pthread_cond_t>,
+    cond: MutPtr<pthread_cond_t>,
     mutex: MutPtr<pthread_mutex_t>,
 ) -> i32 {
-    // ANTI-FREEZE HACK FOR GAMELOFT GAMES:
-    // Instead of permanently parking the thread and waiting for a signal that 
-    // might never arrive (due to bugs in Gameloft's engine or our emulator timing),
-    // we turn this into a yielding spinlock!
-    
-    // 1. Unlock the mutex so the background thread can actually do its work
-    pthread_mutex_unlock(env, mutex);
-    
-    // 2. Force the thread to yield this CPU cycle so we don't freeze the emulator loop
-    env.block_thread(ThreadBlock::Yield);
-    
-    // 3. Relock the mutex immediately. If another thread grabbed it, this call will safely 
-    // queue our thread to wait for the mutex lock natively.
-    pthread_mutex_lock(env, mutex);
-    
-    0 // Return success immediately
+    let res = pthread_mutex_unlock(env, mutex);
+    assert_eq!(res, 0);
+    log_dbg!(
+        "Thread {} is blocking on condition variable {:?}",
+        env.current_thread,
+        cond
+    );
+    let current_thread = env.current_thread;
+    let mutex = env.mem.read(mutex).mutex_id;
+    let cond_var = env.mem.read(cond);
+    let host_object = State::get_mut(env)
+        .condition_variables
+        .get_mut(&cond_var)
+        .unwrap();
+    // The mutex used must be the same as the currently waiting mutex, or there
+    // must be no other waiters.
+    assert!(
+        host_object.curr_mutex == Some(mutex)
+            || host_object.waking.is_empty() && host_object.waiting.is_empty()
+    );
+    host_object.curr_mutex = Some(mutex);
+    host_object.waiting.push_back(current_thread);
+    env.yield_thread(ThreadBlock::Condition(cond_var));
+    0 // success
+}
+
+pub fn pthread_cond_signal(env: &mut Environment, cond: MutPtr<pthread_cond_t>) -> i32 {
+    let cond_var = env.mem.read(cond);
+    let host_object = State::get_mut(env)
+        .condition_variables
+        .get_mut(&cond_var)
+        .unwrap();
+    if let Some(tid) = host_object.waiting.pop_front() {
+        host_object.waking.push_back(tid);
+        log_dbg!(
+            "Thread {} unblocks one thread ({}) waiting on condition variable {:?}",
+            env.current_thread,
+            tid,
+            cond
+        );
+    } else {
+        log_dbg!(
+            "Thread {} signals condition variable {:?}, no waiters",
+            env.current_thread,
+            cond
+        );
+    }
+    0 // success
+}
+
+pub fn pthread_cond_broadcast(env: &mut Environment, cond: MutPtr<pthread_cond_t>) -> i32 {
+    let cond_var = env.mem.read(cond);
+    log_dbg!(
+        "Thread {} unblocks one thread waiting on condition variable {:?}",
+        env.current_thread,
+        cond
+    );
+    let host_object = State::get_mut(env)
+        .condition_variables
+        .get_mut(&cond_var)
+        .unwrap();
+    host_object.waking.extend(host_object.waiting.drain(..));
+    0 // success
+}
+
+pub fn pthread_cond_destroy(env: &mut Environment, cond: MutPtr<pthread_cond_t>) -> i32 {
+    let cond_var = env.mem.read(cond);
+    let old_object = State::get_mut(env)
+        .condition_variables
+        .remove(&cond_var)
+        .unwrap();
+    assert!(old_object.waiting.is_empty() && old_object.waking.is_empty());
+    env.mem.free(cond_var.cast());
+    0 // success
 }
 
 pub fn pthread_cond_timedwait(
@@ -124,6 +153,14 @@ pub fn pthread_cond_timedwait(
     mutex: MutPtr<pthread_mutex_t>,
     _abstime: u32,
 ) -> i32 {
-    // Just use our anti-freeze wait implementation to prevent deadlocks
     pthread_cond_wait(env, cond, mutex)
 }
+
+pub const FUNCTIONS: FunctionExports = &[
+    export_c_func!(pthread_cond_init(_, _)),
+    export_c_func!(pthread_cond_wait(_, _)),
+    export_c_func!(pthread_cond_signal(_)),
+    export_c_func!(pthread_cond_broadcast(_)),
+    export_c_func!(pthread_cond_destroy(_)),
+    export_c_func!(pthread_cond_timedwait(_, _, _)),
+];
