7834f8ae356aa55949c48ad424066018531a81e9
fazidroid <84150589+fazidroid@users.noreply.github.com>
2026-04-06 07:33:34 +0530

    GT Free+ 1.3.4 WIP

--- src/frameworks/foundation/ns_dictionary.rs
+++ src/frameworks/foundation/ns_dictionary.rs
@@ -22,7 +22,7 @@ use crate::frameworks::foundation::ns_file_manager::{
 use crate::fs::GuestPath;
 use crate::mem::{ConstPtr, MutPtr, Ptr, SafeRead};
 use crate::objc::{
-    autorelease, id, msg, msg_class, nil, objc_classes, release, retain, Class, ClassExports,
+    autorelease, id, msg, msg_class, msg_super, nil, objc_classes, release, retain, Class, ClassExports,
     HostObject, NSZonePtr,
 };
 use crate::{impl_HostObject_with_superclass, Environment};
@@ -38,7 +38,8 @@ pub(super) struct DictionaryHostObject {
     /// Since we need custom hashing and custom equality, and these both need a
     /// `&mut Environment`, we can't just use a `HashMap<id, id>`.
     /// So here we are using a `HashMap` as a primitive for implementing a
-    /// hash-map, which is not ideally efficient. :)
+    /// hash-map, which is not ideally efficient.
+    ///
     /// The keys are the hash values, the values are a list of key-value pairs
     /// where the keys have the same hash value.
     pub(super) map: HashMap<Hash, Vec<(id, id)>>,
@@ -67,7 +68,6 @@ impl DictionaryHostObject {
         let hash: Hash = msg![env; key hash];
 
         let value = retain(env, value);
-
         let Some(collisions) = self.map.get_mut(&hash) else {
             self.map.insert(hash, vec![(key, value)]);
             self.count += 1;
@@ -299,7 +299,8 @@ pub fn init_with_objects_and_keys(
             break;
         }
         let key: id = va_args.next(env);
-        assert!(key != nil); // TODO: raise proper exception
+        assert!(key != nil);
+        // TODO: raise proper exception
         host_object.insert(env, key, object, /* copy_key: */ true);
     }
 
@@ -311,7 +312,6 @@ pub fn init_with_objects_and_keys(
 /// Helper function to share `initWithDictionary:` implementations
 fn init_with_dictionary_common(env: &mut Environment, this: id, other_dict: id) -> id {
     let other_host_object: DictionaryHostObject = std::mem::take(env.objc.borrow_mut(other_dict));
-
     let mut host_object = <DictionaryHostObject as Default>::default();
 
     for key in other_host_object.iter_keys() {
@@ -328,7 +328,8 @@ fn init_with_dictionary_common(env: &mut Environment, this: id, other_dict: id) 
 fn init_with_objects_for_keys_common(env: &mut Environment, this: id, objects: id, keys: id) -> id {
     let keys_size: NSUInteger = msg![env; keys count];
     let objects_size: NSUInteger = msg![env; objects count];
-    assert_eq!(keys_size, objects_size); // TODO: raise proper exception
+    assert_eq!(keys_size, objects_size);
+    // TODO: raise proper exception
 
     let mut host_object = <DictionaryHostObject as Default>::default();
 
@@ -368,7 +369,6 @@ fn all_keys_common(env: &mut Environment, this: id) -> id {
 pub const CLASSES: ClassExports = objc_classes! {
 
 (env, this, _cmd);
-
 // NSDictionary is an abstract class. A subclass must provide:
 // - (id)initWithObjects:(id*)forKeys:(id*)count:(NSUInteger)
 // - (NSUInteger)count
@@ -379,10 +379,12 @@ pub const CLASSES: ClassExports = objc_classes! {
 @implementation NSDictionary: NSObject
 
 + (id)allocWithZone:(NSZonePtr)zone {
-    // NSDictionary might be subclassed by something which needs allocWithZone:
-    // to have the normal behaviour. Unimplemented: call superclass alloc then.
-    assert!(this == env.objc.get_known_class("NSDictionary", &mut env.mem));
-    msg_class![env; _touchHLE_NSDictionary allocWithZone:zone]
+    let known_class = env.objc.get_known_class("NSDictionary", &mut env.mem);
+    if this == known_class {
+        msg_class![env; _touchHLE_NSDictionary allocWithZone:zone]
+    } else {
+        msg_super![env; this allocWithZone:zone]
+    }
 }
 
 + (id)dictionary {
@@ -392,7 +394,8 @@ pub const CLASSES: ClassExports = objc_classes! {
 }
 
 + (id)dictionaryWithObject:(id)object forKey:(id)key {
-    assert_ne!(key, nil); // TODO: raise proper exception
+    assert_ne!(key, nil);
+    // TODO: raise proper exception
 
     let new_dict = dict_from_keys_and_objects(env, &[(key, object)]);
     autorelease(env, new_dict)
@@ -509,10 +512,12 @@ pub const CLASSES: ClassExports = objc_classes! {
 @implementation NSMutableDictionary: NSDictionary
 
 + (id)allocWithZone:(NSZonePtr)zone {
-    // NSDictionary might be subclassed by something which needs allocWithZone:
-    // to have the normal behaviour. Unimplemented: call superclass alloc then.
-    assert!(this == env.objc.get_known_class("NSMutableDictionary", &mut env.mem));
-    msg_class![env; _touchHLE_NSMutableDictionary allocWithZone:zone]
+    let known_class = env.objc.get_known_class("NSMutableDictionary", &mut env.mem);
+    if this == known_class {
+        msg_class![env; _touchHLE_NSMutableDictionary allocWithZone:zone]
+    } else {
+        msg_super![env; this allocWithZone:zone]
+    }
 }
 
 + (id)dictionaryWithCapacity:(NSUInteger)capacity {
@@ -566,7 +571,6 @@ pub const CLASSES: ClassExports = objc_classes! {
 
 - (())dealloc {
     std::mem::take(env.objc.borrow_mut::<DictionaryHostObject>(this)).release(env);
-
     env.objc.dealloc_object(this, &mut env.mem)
 }
 
@@ -654,7 +658,6 @@ pub const CLASSES: ClassExports = objc_classes! {
 
 - (())dealloc {
     std::mem::take(env.objc.borrow_mut::<DictionaryHostObject>(this)).release(env);
-
     env.objc.dealloc_object(this, &mut env.mem)
 }
 
@@ -699,7 +702,6 @@ pub const CLASSES: ClassExports = objc_classes! {
     } else {
         unimplemented!()
     };
-
     release(env, this);
     let dict = dict_from_keys_and_objects(env, &tuples);
 
@@ -814,7 +816,6 @@ pub const CLASSES: ClassExports = objc_classes! {
     let host_obj: DictionaryHostObject = std::mem::take(env.objc.borrow_mut(this));
     let values: Vec<id> = host_obj.map.values().flatten().map(|&(_key, value)| value).collect();
     *env.objc.borrow_mut(this) = host_obj;
-
     for &val in &values {
         retain(env, val);
     }
@@ -924,10 +925,10 @@ pub const CLASSES: ClassExports = objc_classes! {
 @end
 
 };
-
 /// Direct constructor for use by host code, similar to
 /// `[[NSDictionary alloc] initWithObjectsAndKeys:]` but without variadics and
-/// with a more intuitive argument order. Unlike [super::ns_array::from_vec],
+/// with a more intuitive argument order.
+/// Unlike [super::ns_array::from_vec],
 /// this **does** copy and retain!
 pub fn dict_from_keys_and_objects(env: &mut Environment, keys_and_objects: &[(id, id)]) -> id {
     let dict: id = msg_class![env; NSDictionary alloc];
