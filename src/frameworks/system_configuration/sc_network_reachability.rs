70af3e11f0c194892cd6619339964f27785fdc75
fazidroid <84150589+fazidroid@users.noreply.github.com>
2026-04-09 00:38:46 +0530

    Update sc_network_reachability.rs

--- src/frameworks/system_configuration/sc_network_reachability.rs
+++ src/frameworks/system_configuration/sc_network_reachability.rs
@@ -6,68 +6,39 @@
 //! SCNetworkReachability.
 
 use crate::dyld::{export_c_func, FunctionExports};
-use crate::mem::MutPtr;
-use crate::objc::ClassExports;
+use crate::mem::{ConstPtr, MutPtr, MutVoidPtr, Ptr};
 use crate::Environment;
 
-pub const CLASSES: ClassExports = &[];
+// Define the missing type so the compiler stops complaining
+pub type SCNetworkReachabilityRef = MutVoidPtr;
 
-fn SCNetworkReachabilityScheduleWithRunLoop(
-    _env: &mut Environment,
-    _target: SCNetworkReachabilityRef,
-    _run_loop: MutVoidPtr,
-    _run_loop_mode: MutVoidPtr,
-) -> bool {
-    // FakeNetSchedule
-    true
+fn SCNetworkReachabilityCreateWithAddress(_env: &mut Environment, _allocator: ConstPtr<u8>, _address: ConstPtr<u8>) -> SCNetworkReachabilityRef {
+    Ptr::from_bits(0xDEADBEEF)
 }
 
-fn SCNetworkReachabilityUnscheduleFromRunLoop(
-    _env: &mut Environment,
-    _target: SCNetworkReachabilityRef,
-    _run_loop: MutVoidPtr,
-    _run_loop_mode: MutVoidPtr,
-) -> bool {
-    // FakeNetUnschedule
-    true
+fn SCNetworkReachabilityCreateWithName(_env: &mut Environment, _allocator: ConstPtr<u8>, _nodename: ConstPtr<u8>) -> SCNetworkReachabilityRef {
+    Ptr::from_bits(0xDEADBEEF)
+}
+
+fn SCNetworkReachabilityGetFlags(_env: &mut Environment, _target: SCNetworkReachabilityRef, flags_out: MutPtr<u32>) -> i32 {
+    if !flags_out.is_null() {
+        _env.mem.write::<u32>(flags_out, 0); 
+    }
+    1 // Return true
+}
+
+fn SCNetworkReachabilityScheduleWithRunLoop(_env: &mut Environment, _target: SCNetworkReachabilityRef, _run_loop: MutVoidPtr, _run_loop_mode: MutVoidPtr) -> i32 {
+    1 // Return true (Absorb safely)
+}
+
+fn SCNetworkReachabilityUnscheduleFromRunLoop(_env: &mut Environment, _target: SCNetworkReachabilityRef, _run_loop: MutVoidPtr, _run_loop_mode: MutVoidPtr) -> i32 {
+    1 // Return true (Absorb safely)
 }
 
 pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(SCNetworkReachabilityCreateWithAddress(_, _)),
     export_c_func!(SCNetworkReachabilityCreateWithName(_, _)),
     export_c_func!(SCNetworkReachabilityGetFlags(_, _)),
-    export_c_func!(SCNetworkReachabilitySetCallback(_, _, _)),
     export_c_func!(SCNetworkReachabilityScheduleWithRunLoop(_, _, _)),
     export_c_func!(SCNetworkReachabilityUnscheduleFromRunLoop(_, _, _)),
 ];
-
-fn SCNetworkReachabilityCreateWithAddress(_env: &mut Environment, _allocator: u32, _address: u32) -> u32 {
-    1 // Return a dummy handle
-}
-
-fn SCNetworkReachabilityCreateWithName(_env: &mut Environment, _allocator: u32, _nodename: u32) -> u32 {
-    1 // Return a dummy handle
-}
-
-fn SCNetworkReachabilityGetFlags(env: &mut Environment, _target: u32, flags_out: MutPtr<u32>) -> u32 {
-    if !flags_out.is_null() {
-        // FIXED: Gameloft Asphalt 6 Loop Bypass!
-        // We write `2` (kSCNetworkReachabilityFlagsReachable) instead of 0.
-        // This forces the game to attempt a socket connection (which we instantly 
-        // kill in netdb.rs), breaking the infinite "Gateway is down" loop!
-        env.mem.write(flags_out, 2u32);
-    }
-    1 // Return 1 (true) to indicate we successfully retrieved the flags
-}
-
-fn SCNetworkReachabilitySetCallback(_env: &mut Environment, _target: u32, _callout: u32, _context: u32) -> u32 {
-    1
-}
-
-fn SCNetworkReachabilityScheduleWithRunLoop(_env: &mut Environment, _target: u32, _runloop: u32, _runloop_mode: u32) -> u32 {
-    1
-}
-
-fn SCNetworkReachabilityUnscheduleFromRunLoop(_env: &mut Environment, _target: u32, _runloop: u32, _runloop_mode: u32) -> u32 {
-    1
-}
