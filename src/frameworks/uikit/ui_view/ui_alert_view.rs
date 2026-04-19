d8c810c9c5f65cfde1d54a1d31544d28fdde4807
fazidroid <84150589+fazidroid@users.noreply.github.com>
2026-04-19 11:38:20 +0530

    NFS MW: Compile Fix

--- src/frameworks/uikit/ui_view/ui_alert_view.rs
+++ src/frameworks/uikit/ui_view/ui_alert_view.rs
@@ -27,36 +27,15 @@ pub const CLASSES: ClassExports = objc_classes! {
     let title = if title == nil { Cow::from("(nil)") } else { ns_string::to_rust_string(env, title) };
     log!("UIAlertView: title: {:?}, message: {:?}", title, msg);
 
-    // Store delegate
-    let host_obj = env.objc.borrow_mut::<UIAlertViewHostObject>(this);
-    host_obj.delegate = delegate;
-    host_obj.button_count = 0;
-
     msg_super![env; this init]
 }
 
 - (())addButtonWithTitle:(id)title {
     log!("TODO: [(UIAlertView *){:?} addButtonWithTitle:{}]", this, ns_string::to_rust_string(env, title));
-    let host_obj = env.objc.borrow_mut::<UIAlertViewHostObject>(this);
-    host_obj.button_count += 1;
 }
 
 - (())show {
-    log!("UIAlertView: auto-dismissing alert");
-    let host_obj = env.objc.borrow_mut::<UIAlertViewHostObject>(this);
-    let delegate = host_obj.delegate;
-    if delegate != nil {
-        // Simulate tapping the first button (index 0)
-        let clicked_sel = env.objc.get_selector("alertView:clickedButtonAtIndex:");
-        if msg![env; delegate respondsToSelector:clicked_sel] {
-            msg![env; delegate alertView:this clickedButtonAtIndex:0];
-        }
-        // Also call didDismiss if implemented
-        let dismissed_sel = env.objc.get_selector("alertView:didDismissWithButtonIndex:");
-        if msg![env; delegate respondsToSelector:dismissed_sel] {
-            msg![env; delegate alertView:this didDismissWithButtonIndex:0];
-        }
-    }
+    log!("TODO: [(UIAlertView*){:?} show]", this);
 }
 
 @end
