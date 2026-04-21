e14e4a9c9ed17639806a346105f7e253db81c443
fazidroid <84150589+fazidroid@users.noreply.github.com>
2026-04-21 23:04:40 +0530

    GT Free+ WIP: New file ns_assertion_handler.rs

---
+++ src/frameworks/foundation/ns_assertion_handler.rs
@@ -0,0 +1,49 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+use crate::objc::{id, nil, objc_classes, ClassExports, sel, SEL};
+
+pub const CLASSES: ClassExports = &[
+    (
+        "NSAssertionHandler",
+        vec![
+            // Class methods
+            sel!(currentHandler) => current_handler,
+            // Instance methods
+            sel!(handleFailureInMethod:object:file:lineNumber:description:) => handle_failure,
+            sel!(handleFailureInFunction:file:lineNumber:description:) => handle_failure_func,
+        ],
+        None,
+    ),
+];
+
+fn current_handler(this: id, _sel: SEL) -> id {
+    log_dbg!("NSAssertionHandler currentHandler called, returning dummy object");
+    this
+}
+
+fn handle_failure(
+    _this: id,
+    _sel: SEL,
+    _method: id,
+    _object: id,
+    _file: id,
+    _line: i32,
+    _description: id,
+) {
+    log_once!("NSAssertionHandler handleFailureInMethod:... ignored");
+}
+
+fn handle_failure_func(
+    _this: id,
+    _sel: SEL,
+    _function: id,
+    _file: id,
+    _line: i32,
+    _description: id,
+) {
+    log_once!("NSAssertionHandler handleFailureInFunction:... ignored");
+}
