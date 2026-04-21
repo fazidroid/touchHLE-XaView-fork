75f3a52b371d89a432de310f0a23e2b9072a2955
fazidroid <84150589+fazidroid@users.noreply.github.com>
2026-04-21 23:15:43 +0530

    Compile fix

--- src/frameworks/foundation.rs
+++ src/frameworks/foundation.rs
@@ -53,7 +53,7 @@ pub mod ns_xml_parser;
 pub mod ns_condition_lock;
 pub mod ns_sort_descriptor;
 pub mod ns_invocation;
-pub mod ns_assertion_handler
+pub mod ns_assertion_handler;
 
 
 pub const DYLIB: crate::dyld::HostDylib = crate::dyld::HostDylib {
