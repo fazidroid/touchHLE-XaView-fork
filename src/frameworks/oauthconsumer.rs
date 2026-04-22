297ba85c68e43aad2ffd243376ecfd8178bdca48
fazidroid <84150589+fazidroid@users.noreply.github.com>
2026-04-22 23:44:00 +0530

    GT Free+ WIP: HTTPMethod patch

--- src/frameworks/oauthconsumer.rs
+++ src/frameworks/oauthconsumer.rs
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
-//! Stubs for OAuthConsumer framework classes.
+//! Stubs for OAuthConsumer framework.
 
 use crate::frameworks::foundation::ns_string;
 use crate::objc::{id, msg, msg_class, nil, objc_classes, ClassExports, HostObject, NSZonePtr};
@@ -11,10 +11,6 @@ use crate::objc::{id, msg, msg_class, nil, objc_classes, ClassExports, HostObjec
 #[derive(Default)]
 struct OAMutableURLRequestHostObject;
 impl HostObject for OAMutableURLRequestHostObject {}
-
-#[derive(Default)]
-struct OAServiceTicketHostObject;
-impl HostObject for OAServiceTicketHostObject {}
 
 pub const CLASSES: ClassExports = objc_classes! {
 
@@ -31,110 +27,30 @@ pub const CLASSES: ClassExports = objc_classes! {
 }
 
 - (id)HTTPMethod {
-    // Return a default HTTP method (e.g., GET)
     let method = ns_string::get_static_str(env, "GET");
     method
 }
 
 - (())setHTTPMethod:(id)method {
-    // Stub - ignore
+    // stub
 }
 
-- (id)URL {
-    return nil;
-}
-
-- (())setURL:(id)url {
-    // Stub
-}
-
-- (id)parameters {
-    return nil;
-}
-
-- (())setParameters:(id)parameters {
-    // Stub
-}
-
-- (id)consumer {
-    return nil;
-}
-
-- (())setConsumer:(id)consumer {
-    // Stub
-}
-
-- (id)token {
-    return nil;
-}
-
-- (())setToken:(id)token {
-    // Stub
-}
-
-- (id)realm {
-    return nil;
-}
-
-- (())setRealm:(id)realm {
-    // Stub
-}
-
-- (id)signature {
-    return nil;
-}
-
-- (())setSignature:(id)signature {
-    // Stub
-}
-
-- (id)oauthParameters {
-    return nil;
-}
-
-- (())setOauthParameters:(id)oauthParameters {
-    // Stub
-}
-
-- (bool)prepared {
-    return false;
-}
-
-- (())prepare {
-    // Stub
-}
-
-- (id)OAuthMutableURLRequest {
-    return this;
-}
-
-@end
-
-@implementation OAServiceTicket: NSObject
-
-+ (id)allocWithZone:(NSZonePtr)_zone {
-    env.objc.alloc_object(this, Box::new(OAServiceTicketHostObject), &mut env.mem)
-}
-
-- (id)initWithRequest:(id)request response:(id)response data:(id)data didSucceed:(bool)didSucceed {
-    msg![env; this init]
-}
-
-- (id)request {
-    return nil;
-}
-
-- (id)response {
-    return nil;
-}
-
-- (id)data {
-    return nil;
-}
-
-- (bool)didSucceed {
-    return true;
-}
+- (id)URL { nil }
+- (())setURL:(id)url {}
+- (id)parameters { nil }
+- (())setParameters:(id)params {}
+- (id)consumer { nil }
+- (())setConsumer:(id)consumer {}
+- (id)token { nil }
+- (())setToken:(id)token {}
+- (id)realm { nil }
+- (())setRealm:(id)realm {}
+- (id)signature { nil }
+- (())setSignature:(id)signature {}
+- (id)oauthParameters { nil }
+- (())setOauthParameters:(id)params {}
+- (bool)prepared { false }
+- (())prepare {}
 
 @end
 
