2f761ad44691379bc03bccc9824abff2569d47af
fazidroid <84150589+fazidroid@users.noreply.github.com>
2026-04-23 22:58:03 +0530

    Real racing 2 WIP: stub initWithMediaTypes

--- src/frameworks/media_player/media_picker_controller.rs
+++ src/frameworks/media_player/media_picker_controller.rs
@@ -12,6 +12,22 @@ pub const CLASSES: ClassExports = objc_classes! {
 (env, this, _cmd);
 
 @implementation MPMediaPickerController: UIViewController
+
+- (id)initWithMediaTypes:(i32)mediaTypes {
+        println!("🎮 LOG: Caught [MPMediaPickerController initWithMediaTypes:{}]. Returning initialized picker!", mediaTypes);
+        // Call the superclass init to properly set up the view controller!
+        crate::msg_super![env; this init]
+    }
+
+    // 🏎️ PROACTIVE STUBS: Catch the configuration settings!
+    - (())setDelegate:(id)delegate {
+        println!("🎮 LOG: Caught [MPMediaPickerController setDelegate:]. Absorbing safely!");
+    }
+    
+    - (())setAllowsPickingMultipleItems:(bool)allows { }
+    
+    - (())setPrompt:(id)prompt { }
+
 // TODO
 @end
 
