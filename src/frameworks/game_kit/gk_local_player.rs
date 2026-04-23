c8c0479f6b99662c76ec5525a414d09a5e8157bc
fazidroid <84150589+fazidroid@users.noreply.github.com>
2026-04-23 21:48:59 +0530

    Real racing 2 WIP: stub localPlayer

--- src/frameworks/game_kit/gk_local_player.rs
+++ src/frameworks/game_kit/gk_local_player.rs
@@ -14,6 +14,25 @@ pub const CLASSES: ClassExports = objc_classes! {
 
 // TODO: proper inheritance chain
 @implementation GKLocalPlayer: NSObject
+
++ (id)localPlayer {
+        println!("🎮 LOG: Caught [GKLocalPlayer localPlayer]. Returning fake player!");
+        let player: id = msg![env; this alloc];
+        let player: id = msg![env; player init];
+        crate::objc::autorelease(env, player)
+    }
+
+    // 🏎️ PROACTIVE STUBS: Game Center Auth Bypasses
+    - (bool)isAuthenticated {
+        println!("🎮 LOG: Caught [GKLocalPlayer isAuthenticated]. Returning false (offline mode)!");
+        false // Tells the game we are not logged into Game Center!
+    }
+
+    - (())authenticateWithCompletionHandler:(id)handler {
+        println!("🎮 LOG: Caught [GKLocalPlayer authenticateWithCompletionHandler:]. Absorbing safely!");
+        // We just absorb this so it doesn't try to pop up a login screen!
+    }
+    
 // TODO
 @end
 
