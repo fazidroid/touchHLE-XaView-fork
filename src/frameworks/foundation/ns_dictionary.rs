7d38034141f9140aa9dd261a9fa48ebe4146aa97
fazidroid <84150589+fazidroid@users.noreply.github.com>
2026-04-23 15:22:43 +0530

    GT racing 2 WIP: update allocWithZone

--- src/frameworks/foundation/ns_dictionary.rs
+++ src/frameworks/foundation/ns_dictionary.rs
@@ -511,7 +511,9 @@ pub const CLASSES: ClassExports = objc_classes! {
 + (id)allocWithZone:(NSZonePtr)zone {
     // NSDictionary might be subclassed by something which needs allocWithZone:
     // to have the normal behaviour. Unimplemented: call superclass alloc then.
-    assert!(this == env.objc.get_known_class("NSMutableDictionary", &mut env.mem));
+    
+    // 🏎️ GAMELOFT BYPASS: Relax strict class check!
+    // assert!(this == env.objc.get_known_class("NSMutableDictionary", &mut env.mem));
     msg_class![env; _touchHLE_NSMutableDictionary allocWithZone:zone]
 }
 
