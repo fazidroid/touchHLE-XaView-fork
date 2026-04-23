67864f40d7a02e4e13f4aebadee45042fe5f54a5
fazidroid <84150589+fazidroid@users.noreply.github.com>
2026-04-23 05:40:07 +0530

    GTR2 WIP: Compile Fix

--- src/frameworks/uikit/ui_screen.rs
+++ src/frameworks/uikit/ui_screen.rs
@@ -104,8 +104,9 @@ pub const CLASSES: ClassExports = objc_classes! {
 - (id)currentMode {
     log!("UIScreen currentMode stub called");
     let bounds: CGRect = msg![env; this bounds];
+    let size = bounds.size;
     let mode: id = msg_class![env; UIScreenMode alloc];
-    let mode: id = msg![env; mode initWithSize:bounds.size pixelAspectRatio:1.0f];
+    let mode: id = msg![env; mode initWithSize:size pixelAspectRatio:1.0f];
     autorelease(env, mode)
 }
 
