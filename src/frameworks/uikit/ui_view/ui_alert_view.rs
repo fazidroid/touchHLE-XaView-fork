53b327912f731341c0569bc4ab66f50c767e5108
fazidroid <84150589+fazidroid@users.noreply.github.com>
2026-05-01 11:49:49 +0530

    RR2 HD WIP: Update ui_alert_view.rs

--- src/frameworks/uikit/ui_view/ui_alert_view.rs
+++ src/frameworks/uikit/ui_view/ui_alert_view.rs
@@ -6,14 +6,26 @@
 //! `UIAlertView`.
 
 use crate::frameworks::foundation::ns_string;
-use crate::objc::{id, msg, msg_super, nil, objc_classes, ClassExports};
+use crate::objc::{id, msg, msg_super, nil, objc_classes, release, retain, ClassExports, HostObject, NSZonePtr};
 use std::borrow::Cow;
+
+struct UIAlertViewHostObject {
+    delegate: id,
+}
+impl HostObject for UIAlertViewHostObject {}
 
 pub const CLASSES: ClassExports = objc_classes! {
 
 (env, this, _cmd);
 
 @implementation UIAlertView: UIView
+
++ (id)allocWithZone:(NSZonePtr)_zone {
+    let host_object = Box::new(UIAlertViewHostObject {
+        delegate: nil,
+    });
+    env.objc.alloc_object(this, host_object, &mut env.mem)
+}
 
 - (id)initWithTitle:(id)title
                       message:(id)message
@@ -22,7 +34,27 @@ pub const CLASSES: ClassExports = objc_classes! {
             otherButtonTitles:(id)otherButtonTitles {
 
     log!("UIAlertView init: title={:?}, msg={:?}", title, message);
+    // Store the delegate passed to init in the host object
+    let host = env.objc.borrow_mut::<UIAlertViewHostObject>(this);
+    if delegate != nil {
+        retain(env, delegate);
+    }
+    host.delegate = delegate;
+
     msg_super![env; this init]
+}
+
+- (id)delegate {
+    env.objc.borrow::<UIAlertViewHostObject>(this).delegate
+}
+
+- (())setDelegate:(id)delegate {
+    let host = env.objc.borrow_mut::<UIAlertViewHostObject>(this);
+    let old = std::mem::replace(&mut host.delegate, delegate);
+    if delegate != old {
+        if delegate != nil { retain(env, delegate); }
+        if old != nil { release(env, old); }
+    }
 }
 
 - (())addButtonWithTitle:(id)title {
@@ -32,7 +64,7 @@ pub const CLASSES: ClassExports = objc_classes! {
 - (())show {
     log!("UIAlertView: AUTO-DISMISS (storage alert bypass)");
 
-    // Retrieve the delegate that was set during init
+    // Retrieve the delegate that was set during init (or later)
     let delegate: id = msg![env; this delegate];
     if delegate != nil {
         let _: () = msg![env; delegate alertView:this clickedButtonAtIndex:0];
