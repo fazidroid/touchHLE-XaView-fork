43afc87dcb5d1158df288b97d51ed9be5c4851fb
fazidroid <84150589+fazidroid@users.noreply.github.com>
2026-05-06 00:48:38 +0530

    AARCH64 WIP: compile fix

--- src/abi.rs
+++ src/abi.rs
@@ -326,18 +326,10 @@ impl_CallFromHost!(0 => P0, 1 => P1, 2 => P2, 3 => P3, 4 => P4, 5 => P5, 6 => P6
 
 /// Calling convention translation for a function argument type.
 pub trait GuestArg: std::fmt::Debug + Sized {
-    /// How many registers does this argument type consume?
     const REG_COUNT: usize;
-
-    /// Read the argument from registers. Only `&regs[0..Self::REG_COUNT]` may
-    /// be accessed.
-    fn from_regs(regs: &[u32]) -> Self;
-
-    /// Write the argument to registers. Only '&mut regs[0..Self::REG_COUNT]`
-    /// may be accessed.
-    fn to_regs(self, regs: &mut [u32]);
+    fn from_regs(regs: &[u64]) -> Self;
+    fn to_regs(self, regs: &mut [u64]);
 }
-
 #[cfg(not(feature = "aarch64"))]
 const MAX_ARG_REGS: usize = 4;
 
@@ -348,11 +340,11 @@ const MAX_ARG_REGS: usize = 8;
 /// argument in order.
 fn read_next_arg<T: GuestArg>(
     reg_offset: &mut usize,
-    regs: &[u32],
-    stack_ptr: ConstPtr<u32>,
+    regs: &[u64],
+    stack_ptr: ConstPtr<u64>,
     mem: &Mem,
 ) -> T {
-    let mut fake_regs = [0u32; 16];
+    let mut fake_regs = [0u64; 16];
     let fake_regs = &mut fake_regs[0..T::REG_COUNT];
 
     for fake_reg in fake_regs.iter_mut() {
@@ -363,7 +355,6 @@ fn read_next_arg<T: GuestArg>(
         }
         *reg_offset += 1;
     }
-
     T::from_regs(fake_regs)
 }
 
@@ -371,11 +362,11 @@ fn read_next_arg<T: GuestArg>(
 /// the sum of the [GuestArg::REG_COUNT]s for all the arguments to be written,
 /// and this will update the stack pointer if necessary, as well as returning
 /// a copy of the original stack pointer so it can be restored later.
-pub fn extend_stack_for_args(reg_count_sum: usize, regs: &mut [u32]) -> u32 {
+pub fn extend_stack_for_args(reg_count_sum: usize, regs: &mut [u64]) -> u64 {
     let old = regs[Cpu::SP];
     if reg_count_sum > MAX_ARG_REGS {
-        let old: ConstPtr<u32> = Ptr::from_bits(old);
-        regs[Cpu::SP] = (old - (reg_count_sum - MAX_ARG_REGS).try_into().unwrap()).to_bits()
+        let old_ptr: ConstPtr<u64> = Ptr::from_bits(old);
+        regs[Cpu::SP] = (old_ptr - (reg_count_sum - MAX_ARG_REGS).try_into().unwrap()).to_bits();
     }
     old
 }
@@ -387,11 +378,11 @@ pub fn extend_stack_for_args(reg_count_sum: usize, regs: &mut [u32]) -> u32 {
 /// appropriately decremented in advance! See [extend_stack_for_args].
 pub fn write_next_arg<T: GuestArg>(
     reg_offset: &mut usize,
-    regs: &mut [u32],
+    regs: &mut [u64],
     mem: &mut Mem,
     arg: T,
 ) {
-    let mut fake_regs = [0u32; 16];
+    let mut fake_regs = [0u64; 16];
     let fake_regs = &mut fake_regs[0..T::REG_COUNT];
     arg.to_regs(fake_regs);
 
@@ -399,7 +390,7 @@ pub fn write_next_arg<T: GuestArg>(
         if *reg_offset < MAX_ARG_REGS {
             regs[*reg_offset] = fake_reg;
         } else {
-            let stack_ptr: MutPtr<u32> = Ptr::from_bits(regs[Cpu::SP]);
+            let stack_ptr: MutPtr<u64> = Ptr::from_bits(regs[Cpu::SP]);
             mem.write(stack_ptr + (*reg_offset - MAX_ARG_REGS).try_into().unwrap(), fake_reg);
         }
         *reg_offset += 1;
@@ -411,23 +402,15 @@ pub fn write_next_arg<T: GuestArg>(
 #[derive(Debug)]
 pub struct DotDotDot(VaList);
 impl DotDotDot {
-    pub fn start(&self) -> VaList {
-        self.0
-    }
+    pub fn start(&self) -> VaList { self.0 }
 }
 
-/// Calling convention translation for a variable arguments list (like C
-/// `va_list`). When used as a function argument, this is equivalent to
-/// passing a `va_list` struct as an argument, e.g. `vprintf()`.
-/// See also [DotDotDot].
 #[derive(Copy, Clone, Debug)]
 pub struct VaList {
     reg_offset: usize,
     stack_pointer: ConstVoidPtr,
 }
 impl VaList {
-    /// Get the next argument, like C's `va_arg()`. Be careful as the type may
-    /// be inferred from the call-site if you don't specify it explicitly.
     pub fn next<T: GuestArg>(&mut self, env: &mut Environment) -> T {
         let sp_reg = self.stack_pointer.cast();
         read_next_arg(&mut self.reg_offset, env.cpu.regs_mut(), sp_reg, &env.mem)
@@ -438,116 +421,56 @@ macro_rules! impl_GuestArg_with {
     ($for:ty, $with:ty) => {
         impl GuestArg for $for {
             const REG_COUNT: usize = <$with as GuestArg>::REG_COUNT;
-            fn from_regs(regs: &[u32]) -> Self {
-                <$with as GuestArg>::from_regs(regs) as $for
-            }
-            fn to_regs(self, regs: &mut [u32]) {
-                <$with as GuestArg>::to_regs(self as $with, regs)
-            }
+            fn from_regs(regs: &[u64]) -> Self { <$with as GuestArg>::from_regs(regs) as $for }
+            fn to_regs(self, regs: &mut [u64]) { <$with as GuestArg>::to_regs(self as $with, regs) }
         }
     };
 }
 
 // GuestArg implementations for u32-like types
 
-impl GuestArg for u32 {
-    const REG_COUNT: usize = 1;
-    fn from_regs(regs: &[u32]) -> Self {
-        regs[0]
-    }
-    fn to_regs(self, regs: &mut [u32]) {
-        regs[0] = self;
-    }
+pub trait GuestRet: std::fmt::Debug + Sized {
+    const SIZE_IN_MEM: Option<GuestUSize> = None;
+    fn from_regs(regs: &[u64]) -> Self { panic!() }
+    fn to_regs(self, regs: &mut [u64]) { panic!() }
+    fn from_mem(ptr: ConstVoidPtr, mem: &Mem) -> Self { panic!() }
+    fn to_mem(self, ptr: MutVoidPtr, mem: &mut Mem) { panic!() }
 }
 
-impl_GuestArg_with!(i32, u32);
-impl_GuestArg_with!(u16, u32);
-impl_GuestArg_with!(i16, u32);
-impl_GuestArg_with!(u8, u32);
-impl_GuestArg_with!(i8, u32);
-
-impl GuestArg for bool {
-    const REG_COUNT: usize = 1;
-    fn from_regs(regs: &[u32]) -> Self {
-        <u32 as GuestArg>::from_regs(regs) != 0
-    }
-    fn to_regs(self, regs: &mut [u32]) {
-        <u32 as GuestArg>::to_regs(self as u32, regs)
-    }
-}
-
-impl GuestArg for f32 {
-    const REG_COUNT: usize = <u32 as GuestArg>::REG_COUNT;
-    fn from_regs(regs: &[u32]) -> Self {
-        Self::from_bits(<u32 as GuestArg>::from_regs(regs))
-    }
-    fn to_regs(self, regs: &mut [u32]) {
-        <u32 as GuestArg>::to_regs(self.to_bits(), regs)
-    }
-}
-
-impl<T, const MUT: bool> GuestArg for Ptr<T, MUT> {
-    const REG_COUNT: usize = 1;
-    fn from_regs(regs: &[u64]) -> Self {
-        Self::from_bits(regs[0])
-    }
-    fn to_regs(self, regs: &mut [u64]) {
-        regs[0] = self.to_bits();
-    }
-}
-
-impl GuestArg for GuestFunction {
-    const REG_COUNT: usize = <ConstVoidPtr as GuestArg>::REG_COUNT;
-    fn from_regs(regs: &[u32]) -> Self {
-        GuestFunction(<ConstVoidPtr as GuestArg>::from_regs(regs))
-    }
-    fn to_regs(self, regs: &mut [u32]) {
-        <ConstVoidPtr as GuestArg>::to_regs(self.0, regs)
-    }
-}
-
-impl GuestArg for VaList {
-    const REG_COUNT: usize = <ConstVoidPtr as GuestArg>::REG_COUNT;
-    fn from_regs(regs: &[u32]) -> Self {
-        // `reg_offset` initialized to 4 as we want to use `stack_pointer` when
-        // calling [read_next_arg]
-        VaList {
-            reg_offset: 4,
-            stack_pointer: <ConstVoidPtr as GuestArg>::from_regs(regs),
+macro_rules! impl_GuestRet_with {
+    ($for:ty, $with:ty) => {
+        impl GuestRet for $for {
+            fn from_regs(regs: &[u64]) -> Self { <$with as GuestRet>::from_regs(regs) as $for }
+            fn to_regs(self, regs: &mut [u64]) { <$with as GuestRet>::to_regs(self as $with, regs) }
         }
-    }
-    fn to_regs(self, _regs: &mut [u32]) {
-        todo!()
-    }
+    };
 }
+
+#[macro_export]
+macro_rules! impl_GuestRet_for_large_struct {
+    ($for:ty) => {
+        impl $crate::abi::GuestRet for $for {
+            const SIZE_IN_MEM: Option<$crate::mem::GuestUSize> = Some($crate::mem::guest_size_of::<$for>());
+            fn from_mem(ptr: $crate::mem::ConstVoidPtr, mem: &$crate::mem::Mem) -> Self { mem.read(ptr.cast::<Self>()) }
+            fn to_mem(self, ptr: $crate::mem::MutVoidPtr, mem: &mut $crate::mem::Mem) { mem.write(ptr.cast::<Self>(), self) }
+        }
+    };
+}
+pub use crate::impl_GuestRet_for_large_struct;
 
 // GuestArg implementations for u64-like types
 
 impl GuestArg for u64 {
-    const REG_COUNT: usize = 2;
-    fn from_regs(regs: &[u32]) -> Self {
-        let mut bytes = [0u8; 8];
-        bytes[0..4].copy_from_slice(&regs[0].to_le_bytes());
-        bytes[4..8].copy_from_slice(&regs[1].to_le_bytes());
-        u64::from_le_bytes(bytes)
-    }
-    fn to_regs(self, regs: &mut [u32]) {
-        let bytes = self.to_le_bytes();
-        regs[0] = u32::from_le_bytes(bytes[0..4].try_into().unwrap());
-        regs[1] = u32::from_le_bytes(bytes[4..8].try_into().unwrap());
-    }
+    const REG_COUNT: usize = 1;
+    fn from_regs(regs: &[u64]) -> Self { regs[0] }
+    fn to_regs(self, regs: &mut [u64]) { regs[0] = self; }
 }
-
 impl_GuestArg_with!(i64, u64);
 
-impl GuestArg for u64 {
+impl GuestArg for f64 {
     const REG_COUNT: usize = 1;
-    fn from_regs(regs: &[u64]) -> Self {
-        regs[0]
-    }
-    fn to_regs(self, regs: &mut [u64]) {
-        regs[0] = self;
-    }
+    fn from_regs(regs: &[u64]) -> Self { Self::from_bits(regs[0]) }
+    fn to_regs(self, regs: &mut [u64]) { regs[0] = self.to_bits(); }
 }
 
 // TODO: Do we need to distinguish arguments from return types, don't they
@@ -623,23 +546,20 @@ macro_rules! impl_GuestRet_for_large_struct {
 pub use crate::impl_GuestRet_for_large_struct; // #[macro_export] is weird...
 
 impl GuestRet for () {
-    fn to_regs(self, _regs: &mut [u32]) {
-        // objc_msgSend (see src/objc/messages.rs) relies on this not touching
-        // the registers, because () will be "returned" after the function it's
-        // meant to be tail-calling.
-    }
-    fn from_regs(_regs: &[u32]) -> Self {}
+    fn to_regs(self, _regs: &mut [u64]) {}
+    fn from_regs(_regs: &[u64]) -> Self {}
 }
 
 // GuestRet implementations for u32-like types
 
+impl GuestRet for f64 {
+    fn from_regs(regs: &[u64]) -> Self { Self::from_bits(regs[0]) }
+    fn to_regs(self, regs: &mut [u64]) { regs[0] = self.to_bits(); }
+}
+
 impl GuestRet for u32 {
-    fn from_regs(regs: &[u32]) -> Self {
-        regs[0]
-    }
-    fn to_regs(self, regs: &mut [u32]) {
-        regs[0] = self;
-    }
+    fn from_regs(regs: &[u64]) -> Self { regs[0] as u32 }
+    fn to_regs(self, regs: &mut [u64]) { regs[0] = self as u64; }
 }
 
 impl_GuestRet_with!(i32, u32);
@@ -649,30 +569,18 @@ impl_GuestRet_with!(u8, u32);
 impl_GuestRet_with!(i8, u32);
 
 impl GuestRet for bool {
-    fn from_regs(regs: &[u32]) -> Self {
-        <u32 as GuestRet>::from_regs(regs) != 0
-    }
-    fn to_regs(self, regs: &mut [u32]) {
-        <u32 as GuestRet>::to_regs(self as u32, regs)
-    }
+    fn from_regs(regs: &[u64]) -> Self { regs[0] != 0 }
+    fn to_regs(self, regs: &mut [u64]) { regs[0] = self as u64; }
 }
 
 impl GuestRet for f32 {
-    fn from_regs(regs: &[u32]) -> Self {
-        Self::from_bits(<u32 as GuestRet>::from_regs(regs))
-    }
-    fn to_regs(self, regs: &mut [u32]) {
-        <u32 as GuestRet>::to_regs(self.to_bits(), regs)
-    }
+    fn from_regs(regs: &[u64]) -> Self { Self::from_bits(regs[0] as u32) }
+    fn to_regs(self, regs: &mut [u64]) { regs[0] = self.to_bits() as u64; }
 }
 
 impl<T, const MUT: bool> GuestRet for Ptr<T, MUT> {
-    fn from_regs(regs: &[u64]) -> Self {
-        Self::from_bits(regs[0])
-    }
-    fn to_regs(self, regs: &mut [u64]) {
-        regs[0] = self.to_bits();
-    }
+    fn from_regs(regs: &[u64]) -> Self { Self::from_bits(regs[0]) }
+    fn to_regs(self, regs: &mut [u64]) { regs[0] = self.to_bits(); }
 }
 
 // GuestRet implementations for u64-like types
