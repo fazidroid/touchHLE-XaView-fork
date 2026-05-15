d720db41a05a098d9b010d187b85b12c66bbe8b2
fazidroid <84150589+fazidroid@users.noreply.github.com>
2026-05-15 16:49:26 +0530

    Add files via upload

--- src/frameworks/ad_support.rs
+++ src/frameworks/ad_support.rs
@@ -1,44 +1,14 @@
 // src/frameworks/ad_support.rs
-
 use crate::dyld::HostDylib;
-use crate::objc::{id, nil, objc_classes, ClassExports, HostObject};
-use crate::Environment;
 
+// This pulls in your fixed as_identifier_manager.rs file
 pub mod as_identifier_manager;
-
-struct ASIdentifierManagerHost;
-impl HostObject for ASIdentifierManagerHost {}
-
-pub const CLASSES: ClassExports = objc_classes! {
-    (env, this, _cmd);
-
-    @implementation ASIdentifierManager: NSObject
-
-        + (id)sharedManager {
-        // 🏎️ TIP: Check your console for this log! 
-        // If it appears, we know the runtime successfully found your class.
-        log!("ASIdentifierManager sharedManager stub called");
-
-        // FIX: Use 'this' directly instead of get_known_class
-        env.objc.alloc_object(this, Box::new(ASIdentifierManagerHost), &mut env.mem)
-    }
-
-    - (id)advertisingIdentifier {
-        // Returning nil is perfectly safe; legacy games have fallbacks for this
-        nil
-    }
-
-    - (bool)isAdvertisingTrackingEnabled {
-        false
-    }
-
-    @end
-};
 
 pub const DYLIB: HostDylib = HostDylib {
     path: "/System/Library/Frameworks/AdSupport.framework/AdSupport",
     aliases: &[],
-    class_exports: &[CLASSES], 
+    // This now exports the classes from the sub-module
+    class_exports: &[as_identifier_manager::CLASSES], 
     function_exports: &[],
     constant_exports: &[],
 };
